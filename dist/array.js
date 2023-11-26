71b839dc4d5048e50833409d5ae233bda4512a66
Eduardo Nunes <eduardo.lima@devsquad.com>
2023-11-26 15:07:06 -0400

    type array

---
+++ dist/array.js
@@ -0,0 +1,7 @@
+"use strict";
+let sortNumbers;
+sortNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(sortNumbers);
+let ids;
+ids = [1, 2, 3, 4, 5, '6', 7, 8, 9, 10];
+console.log(ids);
---
+++ src/array.ts
@@ -0,0 +1,7 @@
+let sortNumbers: Array<number>;
+sortNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(sortNumbers);
+
+let ids: Array<number | string>
+ids = [1, 2, 3, 4, 5, '6', 7, 8, 9, 10];
+console.log(ids);
