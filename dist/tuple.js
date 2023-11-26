35561057627b08a6fcff3531d95dbba500ea83d3
Eduardo Nunes <eduardo.lima@devsquad.com>
2023-11-26 16:19:34 -0400

    type tuple

---
+++ dist/tuple.js
@@ -0,0 +1,10 @@
+"use strict";
+let userData = ['John', 950];
+console.log(userData);
+let users;
+users = [
+    ['John', 'Doe', 950, true],
+    ['Trevor', 'Foster', 550, true],
+    ['Franklin', 'Walker', 760, false],
+];
+console.log(users);
---
+++ src/tuple.ts
@@ -0,0 +1,10 @@
+let userData: [string, number] = ['John', 950];
+console.log(userData);
+
+let users: [string, string, number, boolean][];
+users = [
+    ['John', 'Doe', 950, true],
+    ['Trevor', 'Foster', 550, true],
+    ['Franklin', 'Walker', 760, false],
+];
+console.log(users);
