2b282d13f455351fb04185ea4969f1d737533126
Eduardo Nunes <eduardo.lima@devsquad.com>
2023-11-26 11:24:07 -0400

    fix comment type number

--- src/numbers.ts
+++ src/numbers.ts
@@ -6,7 +6,7 @@ numberExample = 123.456; // Float
 console.log(numberExample);
 numberExample = 0x37CF; // Hexadecimal
 console.log(numberExample);
-numberExample = 0o377; //Octal
+numberExample = 0o377; // Octal
 console.log(numberExample);
 numberExample = 0b11111111; // Binary
 console.log(numberExample);
