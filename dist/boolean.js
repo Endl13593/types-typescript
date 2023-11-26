a43170d616cd0bdc2a37ca30a723c13a00fd055e
Eduardo Nunes <eduardo.lima@devsquad.com>
2023-11-26 11:50:18 -0400

    type boolean

---
+++ dist/boolean.js
@@ -0,0 +1,7 @@
+"use strict";
+let isActive = true;
+isActive = false;
+console.log(isActive);
+let published;
+published = true;
+console.log(published);
---
+++ src/boolean.ts
@@ -0,0 +1,7 @@
+let isActive = true;
+isActive = false;
+console.log(isActive);
+
+let published: boolean;
+published = true;
+console.log(published);
