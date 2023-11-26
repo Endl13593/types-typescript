d57044257f51c3527b77d0ef8410bf747d53859f
Eduardo Nunes <eduardo.lima@devsquad.com>
2023-11-26 11:23:19 -0400

    type number

---
+++ src/numbers.ts
@@ -0,0 +1,12 @@
+let numberExample: number;
+
+numberExample = 123; // Integer
+console.log(numberExample);
+numberExample = 123.456; // Float
+console.log(numberExample);
+numberExample = 0x37CF; // Hexadecimal
+console.log(numberExample);
+numberExample = 0o377; //Octal
+console.log(numberExample);
+numberExample = 0b11111111; // Binary
+console.log(numberExample);
