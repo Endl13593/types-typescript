4860c320c964f4aff9a93d7729b29342e6b2f20b
Eduardo Nunes <eduardo.lima@devsquad.com>
2023-11-26 14:39:29 -0400

    type object

---
+++ dist/object.js
@@ -0,0 +1,10 @@
+"use strict";
+let employee = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    jobTitle: "Web Developer",
+    isMarried: false,
+};
+console.log(employee);
+console.log(employee.jobTitle);
---
+++ src/object.ts
@@ -0,0 +1,15 @@
+let employee: {
+    firstName: string,
+    lastName: string,
+    age: number,
+    jobTitle: string,
+    isMarried: boolean,
+} = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    jobTitle: "Web Developer",
+    isMarried: false,
+}
+console.log(employee);
+console.log(employee.jobTitle);
